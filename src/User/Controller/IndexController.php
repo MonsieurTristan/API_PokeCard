a4df95411697f9e296abc32d4a3ba63ef3ab2733
Tristan K <tristankopec@gmail.com>
2017-12-11 09:51:30 +0100

    Updated API

--- src/User/Controller/IndexController.php
+++ src/User/Controller/IndexController.php
@@ -14,11 +14,28 @@ class IndexController
         $url="https://pokeapi.co/api/v2/pokedex/1/";
         $data = file_get_contents($url);
         $data = json_decode ($data ,true);
-        $json = $data['pokemon_entries'];
-        $json = $data['pokemon_entries'][0];
+        $pokemons = $data['pokemon_entries'];
+
+        $pokemonfr=array();
+
+        foreach ($pokemons as $pokemon) {
+            $urlpokemon = $pokemon['pokemon_species']['url'];
+            $datapokemons = file_get_contents($urlpokemon);
+            $datapokemons = json_decode ($datapokemons ,true);
+            $datapokemons = $datapokemons['names'];
+
+            foreach($datapokemons as $datapokemon){
+              if($datapokemon['language']['name']='fr'){
+                array_push($pokemonfr,$datapokemon['name']);
+              }
+            }
+        }
+        $json = array('name' => $pokemonfr );
         $json = json_encode ($json);
 
         return new Response($json, 200, ['Content-type'=>'application/json']);
     }
+
+
 
 }
